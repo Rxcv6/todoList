d12e8ede8df8aa7796a6b48714afa40fc92ca957
Rxcv6 <161628926+Rxcv6@users.noreply.github.com>
2024-06-27 03:46:04 +0300

    time changed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
+<script lang="ts">
+import dayjs from "dayjs";
+import 'dayjs/locale/ar';
+dayjs.locale('ar');
+let day =dayjs().format('a')=='ص'? 'صباح': 'مساء';
+
+
+
+
+</script>
+
 <div class="p-16 s">
   <header class="flex justify-between">
     <div>
-      <h1 class="text-4xl mb-4">صباح الخير 👋</h1>
-      <h2 class="text-xl text-surface-900/45">اليوم , الاثنين 24</h2>
+      <h1 class="text-4xl mb-4">{day} الخير 👋</h1>
+      <h2 class="text-xl text-surface-900/45">{dayjs().format(' اليوم, dddd D MMM YYYY')}</h2>
     </div>
 
-    <div class="text-red-600 flex justify-end">اليوم</div>
+    <div class="text-red-600 flex justify-end">ddd</div>
   </header>
 </div>
