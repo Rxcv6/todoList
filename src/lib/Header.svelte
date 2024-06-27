7e43e3cd315d445c6abcc37b3df8130028f86121
Rxcv6 <161628926+Rxcv6@users.noreply.github.com>
2024-06-27 03:55:41 +0300

    header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import dayjs from "dayjs";
+  import "dayjs/locale/ar";
+
+  dayjs.locale("ar");
+  let day = dayjs().format("a") == "ص" ? "صباح" : "مساء";
+</script>
+
+<header class="flex justify-between">
+  <div>
+    <h1 class="text-4xl mb-4">{day} الخير 👋</h1>
+    <h2 class="text-xl text-surface-900/45">
+      {dayjs().format(" اليوم, dddd D MMM YYYY")}
+    </h2>
+  </div>
+
+  <div class="bg-red-600 h-12 w-32"></div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,7 @@
 <script lang="ts">
-import dayjs from "dayjs";
-import 'dayjs/locale/ar';
-dayjs.locale('ar');
-let day =dayjs().format('a')=='ص'? 'صباح': 'مساء';
-
-
-
-
+  import Header from "$lib/Header.svelte";
 </script>
 
-<div class="p-16 s">
-  <header class="flex justify-between">
-    <div>
-      <h1 class="text-4xl mb-4">{day} الخير 👋</h1>
-      <h2 class="text-xl text-surface-900/45">{dayjs().format(' اليوم, dddd D MMM YYYY')}</h2>
-    </div>
-
-    <div class="text-red-600 flex justify-end">ddd</div>
-  </header>
+<div class="p-16 ">
+  <Header />
 </div>
